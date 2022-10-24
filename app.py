c0665bda09c7307e3969f2da95bfb7d817ef1999
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-24 19:45:56 -0400

    almost finished number 3

--- app.py
+++ app.py
@@ -6,39 +6,23 @@ app.config['SECRET_KEY'] = 'potato'
 DebugToolbarExtension(app)
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 
-question_number = [-1]
-current_number = [-1]
-currentQuestion = -1
-
-
-
 # storing customer responses
 responses = []
 
 @app.route("/")
 def survey():
-    
-    current_page = -1
-    next_page = current_page + 1
-    current_page = next_page  
-
-    return render_template("survey.html", survey=satisfaction_survey, question = next_page)
+    return render_template("survey.html", survey=satisfaction_survey)
 
 @app.route("/question/<question>")
 def next_question(question):
-    
-    current_page = -1
-    next_page = current_page + 1
-    current_page = next_page
-    
-    return render_template("question.html", survey=satisfaction_survey, question = next_page)
+    return render_template("question.html", survey=satisfaction_survey, page=question)
 
 
-@app.route("/answers", methods=["POST"])
-def answer():
-    answer = request.form["answer"]
-    responses.append(answer)
+# @app.route("/answers", methods=["POST"])
+# def answer():
+#     answer = request.form["answer"]
+#     responses.append(answer)
     
-    return redirect(f"/question/5")
+#     return redirect(f"/question")
     
     
