c91d92745a4ed209f8e71de84009b5151b402d7f
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-24 15:23:13 -0400

    finished number 3

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from surveys import satisfaction_survey
 app = Flask(__name__)
+currentQuestion = -1
 
 # storing customer responses
 responses = []
 
 @app.route("/")
 def survey():
-    return render_template("survey.html")
+    return render_template("survey.html", survey=satisfaction_survey, question=currentQuestion)
+
+@app.route("/question/<question>")
+def next_question(question):
+    question = currentQuestion
+    return render_template("question.html", survey=satisfaction_survey, question=currentQuestion)
+    
