7b54a23915a15204ca2781f4543ee2c9cbab2cd4
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-24 12:42:59 -0400

    working on survey

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# storing customer responses
+responses = []
+
+app.route("/")
+def survey():
+    return render_template("survey.html")
