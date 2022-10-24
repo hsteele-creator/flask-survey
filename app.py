d6362166a336d02052b6fa3e4ae5931a086610a1
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-24 17:05:02 -0400

    working on number 3

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
 from flask import Flask, render_template, redirect, request
+from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'potato'
+DebugToolbarExtension(app)
+DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+question_number = [-1]
+current_number = [-1]
 currentQuestion = -1
+
+
 
 # storing customer responses
 responses = []
 
 @app.route("/")
 def survey():
-    return render_template("survey.html", survey=satisfaction_survey, question=currentQuestion)
+    
+    current_page = -1
+    next_page = current_page + 1
+    current_page = next_page  
+
+    return render_template("survey.html", survey=satisfaction_survey, question = next_page)
 
 @app.route("/question/<question>")
 def next_question(question):
-    question = currentQuestion
-    return render_template("question.html", survey=satisfaction_survey, question=currentQuestion)
+    
+    current_page = -1
+    next_page = current_page + 1
+    current_page = next_page
+    
+    return render_template("question.html", survey=satisfaction_survey, question = next_page)
+
+
+@app.route("/answers", methods=["POST"])
+def answer():
+    answer = request.form["answer"]
+    responses.append(answer)
+    
+    return redirect(f"/question/5")
+    
     
