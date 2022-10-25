b0206944c1500d3162b104804183881e1c9d910a
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-25 15:21:28 -0400

    finished number 7

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'potato'
 DebugToolbarExtension(app)
-DEBUG_TB_INTERCEPT_REDIRECTS = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 # storing customer responses
 responses = []
@@ -15,6 +15,10 @@ def survey():
 
 @app.route("/question/<int:question>")
 def next_question(question):
+    
+    if(len(responses) != question):
+        flash("Sorry, you must answer all of the questions in order")
+        return redirect(f"/question/{len(responses)}")
     
     if question < 4:      
         return render_template("question.html", survey=satisfaction_survey, page=question)
