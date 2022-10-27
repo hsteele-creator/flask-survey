76bee74c2a6822ef6b348e10f3694db279bebca8
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-10-27 11:57:27 -0400

    implemented session to survey

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, request, flash
+from curses import curs_set
+from flask import Flask, render_template, redirect, request, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 app = Flask(__name__)
@@ -16,9 +17,11 @@ def survey():
 @app.route("/question/<int:question>")
 def next_question(question):
     
-    if(len(responses) != question):
+    cur_session = session["responses"]
+    
+    if(len(cur_session) != question):
         flash("Sorry, you must answer all of the questions in order")
-        return redirect(f"/question/{len(responses)}")
+        return redirect(f"/question/{len(cur_session)}")
     
     if question < 4:      
         return render_template("question.html", survey=satisfaction_survey, page=question)
@@ -29,8 +32,15 @@ def next_question(question):
 @app.route("/answers", methods=["POST"])
 def answer():
     answer = request.form["answer"]
-    responses.append(answer)
+    # responses.append(answer)
+    cur_session = session["responses"]
+    cur_session.append(answer)
+    session["responses"] = cur_session
     
-    return redirect(f"/question/{len(responses)}")
-    
+    return redirect(f"/question/{len(cur_session)}")
+
+@app.route("/start_survey", methods=["POST"])
+def start_session():
+    session["responses"] = []
+    return redirect("/question/0")
     
